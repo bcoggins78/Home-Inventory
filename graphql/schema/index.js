ca8fe1094d931e61694b5b88dd67ae5c45fc5a54
bcoggins78 <bcoggins78@gmail.com>
2019-08-05 22:38:49 -0400

    Created schema for graphql

---
+++ graphql/schema/index.js
@@ -0,0 +1,65 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type Item {
+        _id: ID!
+        name: String!
+        serial: String
+        model: String
+        description: String
+        estValue: Float
+        image: String
+        comment: String
+        date: String!
+        creator: User!
+    }
+
+    type User {
+        _id: ID!
+        userName: String!
+        password: String
+        email: String!
+        firstName: String!
+        lastName: String!
+        insName: String
+        insPolicy: String
+        insContact: String
+        createdItems: [Item!]
+    }
+
+    input ItemInput {
+        name: String!
+        serial: String
+        model: String
+        description: String
+        estValue: Float
+        image: String
+        comment: String
+        date: String!
+    }
+
+    input UserInput {
+        userName: String!
+        password: String!
+        email: String!
+        firstName: String!
+        lastName: String!
+        insName: String
+        insPolicy: String
+        insContact: String
+    }
+
+    type RootQuery {
+        items: [Item!]!
+    }
+
+    type RootMutation {
+        createItem(itemInput: ItemInput): Item
+        createUser(userInput: UserInput): User
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`)
