a1995d725929289b1aeca201a748db01602c5a6c
bcoggins78 <bcoggins78@gmail.com>
2019-08-10 18:48:59 -0400

    Removed Date field and added timestamp

--- models/item.js
+++ models/item.js
@@ -1,48 +1,47 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const itemSchema = new Schema({
+const itemSchema = new Schema(
+    {
 
-    name: {
-        type: String,
-        required: true
+        name: {
+            type: String,
+            required: true
+        },
+        serial: {
+            type: String,
+            required: false
+        },
+        model: {
+            type: String,
+            required: false
+        },
+        make: {
+            type: String,
+            required: false
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        estValue: {
+            type: Number,
+            required: false
+        },
+        image: {
+            type: String,
+            required: false
+        },
+        comment: {
+            type: String,
+            required: false
+        },
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    serial: {
-        type: String,
-        required: false
-    },
-    model: {
-        type: String,
-        required: false
-    },
-    make: {
-        type: String,
-        required: false
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    estValue: {
-        type: Number,
-        required: false
-    },
-    image: {
-        type: String,
-        required: false
-    },
-    comment: {
-        type: String,
-        required: false
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model('Item', itemSchema);
