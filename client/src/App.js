2853de2a9b1214386682602d70110a04837ef0bf
bcoggins78 <bcoggins78@gmail.com>
2019-08-24 00:18:37 -0400

    Added profile routes and configured routes to redirect if token is stored

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,41 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import RegisterPage from './pages/Register';
 import LoginPage from './pages/Login';
 import ItemsPage from './pages/Items';
+import ProfilePage from './pages/Profile';
 import NavBar from './components/NavBar/NavBar'
+import AuthContext from './context/auth-context';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  }
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token: token, userId: userId });
+  };
+  logout = () => {
+    this.setState({ token: null, userId: null })
+  }
+
   render() {
     return (
       <BrowserRouter>
-        <NavBar />
-        <main className="main-content">
-          <Switch>
-            <Redirect from="/" to="/login" exact />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/register" component={RegisterPage} />
-            <Route path="/items" component={ItemsPage} />
-          </Switch>
-        </main>
+        <AuthContext.Provider value={{ token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout }}>
+          <NavBar />
+          <main className="main-content">
+            <Switch>
+              {!this.state.token && <Redirect from="/" to="/login" exact />}
+              {!this.state.token && <Redirect from="/items" to="/login" exact />}
+              {!this.state.token && <Redirect from="/profile" to="/login" exact />}
+              {!this.state.token && <Route path="/login" component={LoginPage} />}
+              {!this.state.token && <Route path="/register" component={RegisterPage} />}
+              {this.state.token && <Route path="/profile" component={ProfilePage} />}
+              {this.state.token && <Route path="/items" component={ItemsPage} />}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
       </BrowserRouter>
     );
   }
