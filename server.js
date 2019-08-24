ddcc0c97da53f05ce64729b50d7cdbf56bbaa6ef
bcoggins78 <bcoggins78@gmail.com>
2019-08-23 22:37:12 -0400

    Corrected CORS issue

--- server.js
+++ server.js
@@ -12,11 +12,21 @@ const graphQlResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 
 // Define port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
 app.use(isAuth);
 app.use(logger("dev"));
 
