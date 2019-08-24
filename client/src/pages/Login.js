4fb98dd391311d849b3d70bb074d9c6a3dc74455
bcoggins78 <bcoggins78@gmail.com>
2019-08-23 22:38:04 -0400

    Created logic so that user is authenticated at login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,79 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class LoginPage extends Component {
+
+    state = {
+        isLogin: true
+    }
+
+    constructor(props) {
+        super(props);
+        this.userNameEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const userName = this.userNameEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (userName.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+        
+        let requestBody = {
+            query: `
+                query {
+                    login(userName: "${userName}", password: "${password}") {
+                        userId
+                        token
+                        tokenExpiration
+                    }
+                }`
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 & res.status !== 201) {
+                    throw new Error('Login Failed!');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                console.log(resData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+
+
     render() {
-        return <h1>The Login Page</h1>
+        return (
+            <form className = "login-form" onSubmit = {this.submitHandler}>
+                <div className = "form-input">
+                    <label htmlFor="username">Username</label>
+                    <input type = "username" ref = {this.userNameEl}></input>
+                </div>
+                <div className = "form-input">
+                    <label htmlFor="password">Password</label>
+                    <input type = "password" ref = {this.passwordEl}></input>
+                </div>
+                <div className = "form-submit">
+                    <button type = "submit">Login</button>
+                </div>
+                <span><NavLink to="/register">Sign Up</NavLink></span>
+
+            </form>
+        )
     }
 }
 
