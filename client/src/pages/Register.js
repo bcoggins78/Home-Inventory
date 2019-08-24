5db16fbf3aa38c4ed9ade3b2d1c445e16adf7d00
bcoggins78 <bcoggins78@gmail.com>
2019-08-23 22:37:34 -0400

    Created registration page and configured new user functionality

---
+++ client/src/pages/Register.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class RegisterPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.userNameEl = React.createRef();
+        this.passwordEl = React.createRef();
+        this.emailEl = React.createRef();
+        this.firstNameEl = React.createRef();
+        this.lastNameEl = React.createRef();
+        this.insNameEl = React.createRef();
+        this.insPolicyEl = React.createRef();
+        this.insContactEl = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const userName = this.userNameEl.current.value;
+        const password = this.passwordEl.current.value;
+        const email = this.emailEl.current.value;
+        const firstname = this.firstNameEl.current.value;
+        const lastname = this.lastNameEl.current.value;
+        const insName = this.insNameEl.current.value;
+        const insPolicy = this.insPolicyEl.current.value;
+        const insContact = this.insContactEl.current.value;
+
+        let requestBody = {
+            query: `
+                mutation {
+                    createUser(userInput: {
+                        userName: "${userName}",
+                        password: "${password}",
+                        email: "${email}",
+                        firstName: "${firstname}",
+                        lastName: "${lastname}",
+                        insName: "${insName}",
+                        insPolicy: "${insPolicy}",
+                        insContact: "${insContact}",
+                        }) {
+                            _id
+                            userName
+                        }
+                }`
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (res.status !== 200 & res.status !== 201) {
+                    throw new Error('User Create Failed!');
+                }
+                return res.json();
+            })
+            .then(resData => {
+                console.log(resData);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    render() {
+        return (<form className="login-form" onSubmit={this.submitHandler}>
+            <div className="form-input">
+                <label htmlFor="username">Username</label>
+                <input type="username" ref={this.userNameEl}></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="password">Password</label>
+                <input type="password" ref={this.passwordEl}></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="email">Email</label>
+                <input type="email" ref={this.emailEl}></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="firstname">First Name</label>
+                <input type="text" ref={this.firstNameEl} name = "firstname"></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="lastname">Last Name</label>
+                <input type="text" ref={this.lastNameEl} name = "lastname"></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="insname">Insurance Company</label>
+                <input type="text" ref={this.insNameEl} name = "insname"></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="inspolicy">Insurance Policy Number</label>
+                <input type="text" ref={this.insPolicyEl} name = "inspolicy"></input>
+            </div>
+            <div className="form-input">
+                <label htmlFor="insnumber">Insurance Phone Number</label>
+                <input type="text" ref={this.insContactEl} name = "insnumber"></input>
+            </div>
+
+
+            <div className="form-submit">
+                <button type="submit">Register</button>
+            </div>
+            <div className="form-submit">
+                <button type="reset">Reset</button>
+            </div>
+            <span>Already have an account? <NavLink to="/login">Login</NavLink></span>
+
+        </form>)
+    }
+}
+
+export default RegisterPage;
