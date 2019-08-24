65d7e2ade5e6248f95d7cedab1d3cc401a94f23b
bcoggins78 <bcoggins78@gmail.com>
2019-08-24 00:17:58 -0400

    Updated links to show / hide depending if a token is stored

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthContext from '../../context/auth-context';
 
 import './NavBar.css';
 
 const navBar = props => (
-    <header className="main-nav">
-        <div className="logo">
-            <h1>Home Inventory</h1>
-        </div>
-        <nav className="main-nav-links">
-            <ul>
-                <li><NavLink to="/register">Sign Up</NavLink></li>
-                <li><NavLink to="/login">Login</NavLink></li>
-                <li><NavLink to="/items">Inventory</NavLink></li>
-            </ul>
-        </nav>
-    </header>
+    <AuthContext.Consumer>
+        {(context) => {
+            return (
+                <header className="main-nav">
+                    <div className="logo">
+                        <h1>Home Inventory</h1>
+                    </div>
+                    <nav className="main-nav-links">
+                        <ul>
+                            {!context.token && <li><NavLink to="/register">Sign Up</NavLink></li>}
+                            {!context.token && <li><NavLink to="/login">Login</NavLink></li>}
+                            {context.token && <li><NavLink to="/profile">Profile</NavLink></li>}
+                            {context.token && <li><NavLink to="/items">Inventory</NavLink></li>}
+                        </ul>
+                    </nav>
+                </header>
+            )
+        }}
+
+    </AuthContext.Consumer>
 );
 
 export default navBar;
