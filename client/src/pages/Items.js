14daaf22b39f6ba65f64747c2a376ca29ad558aa
bcoggins78 <bcoggins78@gmail.com>
2019-08-24 20:18:30 -0400

    Added logic for opening Modal

--- client/src/pages/Items.js
+++ client/src/pages/Items.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Items.css'
+
 class ItemsPage extends Component {
+
+    state = {
+        modal: false
+    };
+
+    startCreateItemHandler = () => {
+        this.setState({modal: true});
+    }
+
+    modalConfirmHandler = () => {};
+
+    modalCancelHandler = () => {
+        this.setState({modal: false});
+    }
+
     render() {
-        return <h1>The Inventory Page</h1>
+        return (
+            <React.Fragment>
+                {this.state.modal && <Backdrop />}
+                {this.state.modal && <Modal title="Add Item" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+                    <p>Modal Content</p>
+                </Modal>}
+                <div className="items-control">
+                    <p>Add Items!</p>
+                    <button className="btn" onClick={this.startCreateItemHandler}>Add Item</button>
+                </div>
+            </React.Fragment>
+        )
     }
 }
 
