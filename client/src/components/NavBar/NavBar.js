1af3278bbacc7c951b3e2cf1f9a3935e1a97c383
bcoggins78 <bcoggins78@gmail.com>
2019-08-24 20:19:48 -0400

    Added Logout and updated routes so you are redirected to items page after login

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -14,10 +14,10 @@ const navBar = props => (
                     </div>
                     <nav className="main-nav-links">
                         <ul>
-                            {!context.token && <li><NavLink to="/register">Sign Up</NavLink></li>}
-                            {!context.token && <li><NavLink to="/login">Login</NavLink></li>}
+                            {!context.token && (<li><NavLink to="/register">Sign Up</NavLink></li>)}
+                            {!context.token && (<li><NavLink to="/login">Login</NavLink></li>)}
                             {context.token && <li><NavLink to="/profile">Profile</NavLink></li>}
-                            {context.token && <li><NavLink to="/items">Inventory</NavLink></li>}
+                            {context.token && (<React.Fragment><li><NavLink to="/items">Inventory</NavLink></li><li><button onClick={context.logout}>logout</button></li></React.Fragment>)}
                         </ul>
                     </nav>
                 </header>
