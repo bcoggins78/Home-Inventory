81aebde4e344b17d0ee55bdbcfd9b9139d5095f3
bcoggins78 <bcoggins78@gmail.com>
2019-08-26 01:14:17 -0400

    Created inventory item component moved from items page

---
+++ client/src/components/Items/ItemList/InventoryItem/InventoryItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './InventoryItem.css';
+
+const item = props => (
+    <li key={props.itemId} className="items-list-item">
+        <div className="image-container">
+            <a href={props.image} target="_blank" rel="noopener noreferrer"><img className="item-image" src={props.image} alt={props.name} /></a>
+        </div>
+        <div>
+            <h1>{props.name}</h1>
+            <p>Estimated Value: ${props.estValue}</p>
+        </div>
+        <div>
+            <button className='btn' onClick={props.onDetail.bind(this, props.itemId)}>View Details</button>
+        </div>
+    </li>
+);
+
+export default item
