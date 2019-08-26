5bea99e8e086b21bdb82f482d30eaeeb39bb64a0
bcoggins78 <bcoggins78@gmail.com>
2019-08-26 01:11:29 -0400

    Updated name and styling on logout button

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -10,14 +10,14 @@ const navBar = props => (
             return (
                 <header className="main-nav">
                     <div className="logo">
-                        <h1>Home Inventory</h1>
+                        <h1>Home Inventory Management</h1>
                     </div>
                     <nav className="main-nav-links">
                         <ul>
                             {!context.token && (<li><NavLink to="/register">Sign Up</NavLink></li>)}
                             {!context.token && (<li><NavLink to="/login">Login</NavLink></li>)}
                             {context.token && <li><NavLink to="/profile">Profile</NavLink></li>}
-                            {context.token && (<React.Fragment><li><NavLink to="/items">Inventory</NavLink></li><li><button onClick={context.logout}>logout</button></li></React.Fragment>)}
+                            {context.token && (<React.Fragment><li><NavLink to="/items">Inventory</NavLink></li><li><button className="btn" onClick={context.logout}>Logout</button></li></React.Fragment>)}
                         </ul>
                     </nav>
                 </header>
