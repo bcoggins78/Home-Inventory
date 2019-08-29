5b549005eb234177600e4bc6dc637afba3346922
bcoggins78 <bcoggins78@gmail.com>
2019-08-29 10:52:42 -0400

    started creating logic to show user information, currently commented out.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 
+// import authContext from '../context/auth-context';
+
+import './Profile.css';
+
 class ProfilePage extends Component {
+
+    // state = {
+    //     firstName: "",
+    //     lastName: "",
+    //     email: "",
+    //     insName: "",
+    //     insPolicy: "",
+    //     insContact: "",
+    // }
+
+    // static contextType = authContext;
+
+    // componentDidMount() {
+    //     this.renderUser();
+    // }
+
+
     render() {
         return <h1>The Profile Page</h1>
     }
